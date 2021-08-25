4e1d1b7e2161b5d3ef4db05501ffb7d8eefa4f98
Ivan Busygin <bi-25@yandex.ru>
2021-08-25 18:01:59 +0300

    Занятие 4: Знакомство с DOM

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+	let UL = document.createElement('ul')
+	for (let it of friends) {
+		let LI = document.createElement('li')
+		LI.textContent = `${it.firstName} ${it.lastName}`;
+		UL.append(LI)
+	}
+	return UL
 }
