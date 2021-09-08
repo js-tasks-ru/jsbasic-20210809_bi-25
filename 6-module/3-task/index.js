8cc0f81439edbd536f8be6fb453c6c9095ce7217
Ivan Busygin <bi-25@yandex.ru>
2021-09-08 22:47:43 +0300

    Занятие 6: JS классы и компоненты

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,29 +3,27 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
-    console.log('количество карточек ' + this.slides.length);
+    this.poz = 0;
+    this.render();
+    this.addEvent();
+  }
 
-    const carousel = document.createElement('div');
-    carousel.classList.add('carousel');
-
-    const arrowR = createElement(`
+  render() {
+    this.carousel = createElement(`
+<div class="carousel">
       <div class="carousel__arrow carousel__arrow_right">
         <img src="/assets/images/icons/angle-icon.svg" alt="icon">
       </div>
-    `);
-    const arrowL = createElement(`
       <div class="carousel__arrow carousel__arrow_left">
         <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
       </div>
+      <div class="carousel__inner">
+      </div>
+</div>
     `);
-    carousel.append(arrowR); // добавление стрелок в документ
-    carousel.append(arrowL);
-
-    const carousel__inner = document.createElement('div');
-    carousel__inner.classList.add("carousel__inner");
 
     // отрисовка слайдов
-    for (const slide of slides) {
+    for (const slide of this.slides) {
       const elemCarousel = createElement(`
           <div class="carousel__slide" data-id="${slide.id}">
         <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
@@ -38,74 +36,57 @@ export default class Carousel {
         </div>
       </div>
     `);
-      carousel__inner.append(elemCarousel);
+      this.query('inner').append(elemCarousel);
 
     }
+    this.recreate();
+  } // конец render
 
-    carousel.append(carousel__inner); // Добавление слайдов в документ
-
-    //прячу все слайды кроме первого[0]
-    let allSlides = carousel.querySelectorAll('.carousel__slide');
-    for (let i = 1; i < allSlides.length; i++) {
-      allSlides[i].hidden = true;
-    }
-
-    // события для стрелок
-    let clickRight = carousel.querySelector('.carousel__arrow_right');
-    let clickLeft = carousel.querySelector('.carousel__arrow_left');
-    clickRight.addEventListener('click', actRight);
-    clickLeft.addEventListener('click', actLeft);
-    clickLeft.style.display = 'none';
-
-    let poz = 0;
-
-    function actRight() {
-      allSlides[poz].hidden = true;
-      poz++;
-      allSlides[poz].hidden = false;
-
-      if (poz === 3) {
-        clickRight.style.display = 'none';
-      }
-      if (poz !== 0) {
-        clickLeft.style.display = '';
-      }
-    }
-
-    function actLeft() {
-      allSlides[poz].hidden = true;
-      poz--;
-      allSlides[poz].hidden = false;
-
-      if (poz === 0) {
-        clickLeft.style.display = 'none';
-      }
-      if (poz !== 3) {
-        clickRight.style.display = '';
-      }
-    }
-
-    // событие, добавление товара в корзину
-    const button = carousel.querySelector('.carousel__inner');
-    button.addEventListener('click', choice);
-
-    function choice() {
-      if (event.target.closest('button')) {
-        const id = (event.target.closest('.carousel__slide')).dataset.id;
-
+  addEvent() {
+    this.carousel.onclick = ({target}) => {
+      if (target.closest('.carousel__button')) {
+        let ID = target.closest('[data-id]').dataset.id;
         let ev = new CustomEvent("product-add", {
-          detail: id,
-          bubbles: true
+          detail: ID,
+          bubbles: true,
         });
-        button.dispatchEvent(ev);
-
+        this.carousel.dispatchEvent(ev);
       }
+
+      if (target.closest('.carousel__arrow_right')) {
+        this.poz++;
+        this.recreate();
+      }
+
+      if (target.closest('.carousel__arrow_left')) {
+        this.poz--;
+        this.recreate();
+      }
+    };
+  }
+
+  recreate() {
+    let offset = -this.carousel.offsetWidth * this.poz;
+    let aa = this.carousel.querySelector('.carousel__inner');
+    aa.style.transform = `translateX(${offset}px)`;
+    if (this.poz == this.slides.length - 1) {
+      this.query('arrow_right').style.display = 'none';
+    } else {
+      this.query('arrow_right').style.display = '';
     }
 
-    this._elem = carousel;
+    if (this.poz == 0) {
+      this.query('arrow_left').style.display = 'none';
+    } else {
+      this.query('arrow_left').style.display = '';
+    }
+  }
+
+  query(str) {
+    return this.carousel.querySelector(`.carousel__${str}`);
   }
 
   get elem() {
-    return this._elem;
+    return this.carousel;
   }
 }
