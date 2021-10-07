23025a052641fcc2309a8205af927bec637fad8f
Ivan Busygin <bi-25@yandex.ru>
2021-10-07 20:38:50 +0300

    Занятие 7: Сложные компоненты

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,104 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.render();
+    this.addEvent();
   }
+
+  render() {
+    this.RibbonMenu = createElement(`
+    <div class="ribbon">
+      <!--Кнопка прокрутки влево-->
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    <nav class="ribbon__inner"> </nav>
+      <!--Кнопка прокрутки вправо-->
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    </div>
+`);
+    for (const item of this.categories) {
+      const menu = createElement(`
+        <a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>
+      `);
+      this.query('inner').append(menu);
+    }
+    this.query('item').classList.add('ribbon__item_active');
+  } //конец render
+
+  addEvent() {
+    this.RibbonMenu.addEventListener('click', (event) => {
+      let tar = event.target;
+      if (tar.closest('.ribbon__arrow_left')) {
+        this.moveLeft();
+      }
+      if (tar.closest('.ribbon__arrow_right')) {
+        this.moveRight();
+      }
+
+      if (tar.closest('.ribbon__item')) {
+        event.preventDefault();
+        let active = this.query('item_active')
+        if (active) {
+          active.classList.remove('ribbon__item_active');
+        }
+        tar.classList.add('ribbon__item_active');
+
+        // let ID = event.target.closest('[data-id]').dataset.id;
+        let ID = tar.dataset.id;
+        this.RibbonMenu.dispatchEvent(
+          new CustomEvent('ribbon-select', {
+            detail: ID,
+            bubbles: true,
+          })
+        );
+      }
+    });
+
+    this.query('inner').addEventListener('scroll', (event) => this.update(event));
+  }
+
+  moveLeft() {
+    this.query('inner').scrollBy(-350, 0);
+  }
+
+  moveRight() {
+    this.query('inner').scrollBy(350, 0);
+  }
+
+  update() {
+    let arrowLeft = this.query('arrow_left');
+    let arrowRight = this.query('arrow_right');
+
+    if (this.scrollLeft() < 1) {
+      arrowLeft.classList.remove('ribbon__arrow_visible');
+    } else {
+      arrowLeft.classList.add('ribbon__arrow_visible');
+    }
+
+    if (this.scrollRight() < 1) {
+      arrowRight.classList.remove('ribbon__arrow_visible');
+    } else {
+      arrowRight.classList.add('ribbon__arrow_visible');
+    }
+  }
+
+  scrollLeft() {
+    return this.query('inner').scrollLeft;
+  }
+
+  scrollRight() {
+    return this.query('inner').scrollWidth - (this.query('inner').scrollLeft + this.query('inner').clientWidth);
+  }
+
+  query(str) {
+    return this.RibbonMenu.querySelector(`.ribbon__${str}`);
+  }
+
+  get elem() {
+    return this.RibbonMenu;
+  }
+
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,106 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
-  constructor({ steps, value = 0 }) {
+  constructor({steps, value = 0}) {
+    this.steps = steps;
+    this.render();
+    this.addEvent();
+    this.start(value);
+  }
+
+  render() {
+    this.Slider = createElement(`
+<div class="slider">
+  <div class="slider__thumb">
+    <span class="slider__value"></span>
+  </div>
+  <div class="slider__progress"></div>
+  <div class="slider__steps">
+    ${'<span></span>'.repeat(this.steps)}
+  </div>
+</div>
+    `);
+  }
+
+  addEvent() {
+    this.Slider.ondragstart = () => false;
+    this.Slider.addEventListener('click', this.onClick);
+    this.query('thumb').addEventListener('pointerdown', () => {
+      this.Slider.classList.add('slider_dragging');
+      document.addEventListener('pointermove', this.onMove);
+      document.addEventListener('pointerup', this.onUp);
+    });
+  }
+
+  onClick = (event) => {
+    this.start(this.position(this.rate(event)));
+    this.Slider.dispatchEvent(new CustomEvent('slider-change', {detail: this.value, bubbles: true}));
+  };
+
+  start = (value) => {
+    this.sliderSteps = this.Slider.querySelectorAll('.slider__steps span');
+    this.value = value;
+    this.segment = this.steps - 1;
+
+    const active = this.query('step-active');
+    if (active) {
+      active.classList.remove('slider__step-active');
+    }
+    this.sliderSteps[value].classList.add('slider__step-active');
+
+    let percent = (value / this.segment) * 100;
+    this.query('thumb').style.left = `${percent}%`;
+    this.query('progress').style.width = `${percent}%`;
+    this.query('value').textContent = value;
+  };
+
+  onMove = (event) => {
+    event.preventDefault();
+    let rate = this.rate(event);
+    this.value = this.position(rate);
+    this.query('thumb').style.left = `${rate * 100}%`;
+    this.query('progress').style.width = `${rate * 100}%`;
+    this.query('value').textContent = this.value;
+
+    const active = this.query('step-active');
+    if (active) {
+      active.classList.remove('slider__step-active');
+    }
+    this.sliderSteps[this.value].classList.add('slider__step-active');
+  };
+
+  onUp = (event) => {
+    event.preventDefault();
+    this.start(this.position(this.rate(event)));
+    this.Slider.classList.remove('slider_dragging');
+
+    this.Slider.dispatchEvent(
+      new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      })
+    );
+    document.removeEventListener('pointermove', this.onMove);
+    document.removeEventListener('pointerup', this.onUp);
+  };
+
+  position(rate) {
+    let point = Math.round(rate * this.segment);
+    return point;
+  }
+
+  rate = (event) => {
+    let res = (event.clientX - this.Slider.getBoundingClientRect().x) / this.Slider.offsetWidth;
+    res = (res < 0) ? 0 : res;
+    res = (res > 1) ? 1 : res;
+    return res;
+  };
+
+  query(str) {
+    return this.Slider.querySelector(`.slider__${str}`);
+  }
+
+  get elem() {
+    return this.Slider;
   }
 }
