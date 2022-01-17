86541e8363ef17127b9cc3c5b6a806da888a6e4b
Ivan Busygin <bi-25@yandex.ru>
2022-01-17 17:53:09 +0300

    Finish

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,10 @@
+import {resolve} from "karma/lib/plugin";
+
 export default function promiseClick(button) {
   // ваш код...
+  return new Promise((resolve) => {
+    button.addEventListener('click', event => {
+      resolve(event);
+    }, { once: true } );
+  });
 }
